092874522dfd5979346099a3919ca3c8cf44c5e8
afterglow18 <afterglowtanning18@gmail.com>
2026-07-30 10:25:40 +0000

    Fix hero image: convert PNG to real JPEG, center object-position

--- artifacts/outfit-generator/src/pages/welcome.tsx
+++ artifacts/outfit-generator/src/pages/welcome.tsx
@@ -191,7 +191,7 @@ export default function WelcomePage({ onEnter }: Props) {
           style={{
             position: "absolute", inset: 0,
             width: "100%", height: "100%",
-            objectFit: "cover", objectPosition: "center top",
+            objectFit: "cover", objectPosition: "center center",
             userSelect: "none", pointerEvents: "none",
           }}
         />
